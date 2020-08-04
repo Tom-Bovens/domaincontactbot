24569f5f68d512d4c1cc0af529c5c382db0a233b
Tom-Bovens <tombovens2003@gmail.com>
2020-08-04 16:08:53 +0200

    Added more lines

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 
 
-// To-do
-// Make a good save/load system that allows an agent to come back to a conversation that has since been closed.
-    // Editing the videoContentDescription on Video 6 once it is unprivatised.
-
-    const path = require('path');
+const path = require('path');
 const envfile = `${process.cwd()}${path.sep}.env`;
 require('dotenv').config({
     path: envfile
@@ -29,6 +25,34 @@ const errorCatch = (error) => {
     log(error);
 }
 
+const depositQuote = async () => {
+    let quote = (Math.floor((Math.random() * 100)))
+    let conversation
+    const oldConvFinder = await bot.conversations.list({ name:'Need for inspiration?', participants:{ user: userId } })
+    if (oldConvFinder.length > 0) {
+        conversation = oldConvFinder[0]
+        await bot.send(conversation.id, [
+            {
+                text: quote,
+                isBackchannel: false,
+                role: 'bot',
+                delay: incrementor.set(3)
+            }
+        ])
+    } else {
+        conversation = await bot.conversations.create(
+            { name: 'Need some inspiration?', messages: [{ text: `Hey there. I couldn't find a conversation to drop your quote in. So i made a new one. Here's your quote.` }] }
+        )
+        await bot.send(conversation.id, [
+            {
+                text: quote,
+                isBackchannel: false,
+                role: 'bot',
+                delay: incrementor.set(3)
+            }
+        ])
+    }
+}
 
 // Create a new bot instance
 const bot = new ChipChat({
@@ -41,7 +65,6 @@ if (!process.env.TOKEN) {
     throw 'No token found, please define a token with export TOKEN=(Webhook token), or use an .env file.'
 }
 
-
 // Use any REST resource
 // bot.users.get(bot.auth.user).then((botUser) => {
 // log(`Hello ${botUser.role}`);
@@ -50,30 +73,30 @@ if (!process.env.TOKEN) {
 // Logs any error produced to the console
 bot.on('error', log);
 
-bot.on('user.create', async (loginUser) => {
+bot.on('user.login', async (loginUser) => {
     try {
         const user = await bot.users.get(get(loginUser, 'data.user.id'))
         const userId = user.id
-        const hasSeenGuide = get(user, 'meta.hasSeenCommentguide', 'false')
+        const hasSeenGuide = get(user, 'meta.subscribedToQuotes', 'false')
         log(`User guide status: ${hasSeenGuide}`)
         if (hasSeenGuide === "false") {
             log("User has not seen the guide on result comments yet.")
-            if (user.role == 'guest') {
+            if (user.role == 'agent') {
                 if (userId) {
                     let conversation
-                    const oldConvFinder = await bot.conversations.list({ name:'Result comments', participants:{ user: userId } })
+                    const oldConvFinder = await bot.conversations.list({ name:'Need for inspiration?', participants:{ user: userId } })
                     if (oldConvFinder.length > 0) {
                         conversation = oldConvFinder[0]
                         await bot.send(conversation.id, [
                             {
-                                text: `Hey there ${user.givenName}! Welcome to Chatshipper! I need to show you something. Tell me when you're ready!`,
+                                text: `Hey there. Zen Chip here.`,
                                 isBackchannel: false,
                                 role: 'bot',
                                 delay: incrementor.set(3),
                                 actions: [
                                     {
                                         type: "reply",
-                                        text: "Ok!",
+                                        text: "Continue",
                                         payload: "userReturned"
                                     }
                                 ]
@@ -81,37 +104,59 @@ bot.on('user.create', async (loginUser) => {
                         ])
                     } else {
                         conversation = await bot.conversations.create(
-                            { name: 'Result comments', messages: [{ text: `Hey there ${user.givenName}! I need to show you something!` }] }
+                            { name: 'Need some inspiration?', messages: [{ text: `Hey there. Zen Chip here.` }] }
                         )
                     }
-                    await bot.send(conversation.id, {
-                        type: 'command',
-                        text: '/assign',
-                        meta: {
-                            "users": [
-                                userId
-                            ]
-                        }
-                    })
                     await bot.send(conversation.id, [
                         {
-                            text: `Tell me when you're ready!`,
-                            isBackchannel: false,
+                            type: 'command',
+                            text: '/assign',
+                            meta: {
+                                "users": [
+                                    userId
+                                ]
+                            }
+                        },
+                        {
+                            text: 'I was created to serve you an inspirational quote on a daily basis.',
                             role: 'bot',
-                            delay: incrementor.set(3),
+                            delay: incrementor.increment(4)
+                        },
+                        {
+                            text: 'Something like: "You must do the things you think you cannot do."',
+                            role: 'bot',
+                            delay: incrementor.increment(4)
+                        },
+                        {
+                            text: 'Cool right? I guess we can always use an inspirational quote.',
+                            role: 'bot',
+                            delay: incrementor.increment(4)
+                        },
+                        {
+                            text: 'Does a daily fresh quote sound good to you?',
+                            role: 'bot',
+                            delay: incrementor.increment(4),
                             actions: [
                                 {
                                     type: "reply",
-                                    text: "Ok!",
-                                    payload: "userReturned"
+                                    text: "Yeah, surprise me daily.",
+                                    payload: "userAccepted"
+                                },
+                                {
+                                    type: "reply",
+                                    text: "No thanks.",
+                                    payload: "userDenied"
                                 }
                             ]
                         }
                     ])
                 }
             } else {
-                log(`User is not a guest. Role is : ${user.role}`)
+                log(`User is not an agent. Role is : ${user.role}`)
             }
+        } else if (hasSeenGuide === "true") {
+            depositQuote()
+
         }
     } catch (e){
         errorCatch(e)
@@ -120,30 +165,27 @@ bot.on('user.create', async (loginUser) => {
 
 bot.on('message.create.bot.postback.agent', async (message, conversation) => {
     try {
-        await bot.send(conversation.id, [
-            {
-                text: `Chatshipper has a nice feature called result commenting, which can be used to leave feedback on forms your colleagues made!`,
-                isBackchannel: false,
-                role: 'bot',
-                delay: incrementor.increment(3)
-            },
-            {
-                text: `Just a second, i think i have an example of it somewhere...`,
-                isBackchannel: false,
-                role: 'bot',
-                delay: incrementor.increment(3)
-            },
-            {
-                contentType: 'image/png',
-                text: 'https://cht.onl/a/x7zEKfz3X/commentonform.gif',
-                delay: incrementor.increment(4)
-            }
-        ])
-} catch (e){
-    errorCatch(e)
-}
+        if (message.text == "userAccepted") {
+            await bot.send(conversation.id, [
+                {
+                    text: `Great, i'll be back with a new quote tomorrow.`,
+                    isBackchannel: false,
+                    role: 'bot',
+                    delay: incrementor.increment(3)
+                }
+            ])
+        } else if (message.text == "userDenied") {
+            await bot.send(conversation.id, [
+                {
+                    text: `Ok, i won't bother you again. If you change your mind you can activate me with the >zen command.`,
+                    isBackchannel: false,
+                    role: 'bot',
+                    delay: incrementor.increment(3)
+                }
+            ])
+        }
+    } catch (e) { errorCatch(e) }
 });
-
 
 // Start Express.js webhook server to start listening
 bot.start();
